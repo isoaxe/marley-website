049aff1f9e5cc97c4c87ccc83263b2617a2b1777
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-01 19:25:27 +0100

    Add heading to home-projects
    A title for the project section made fully responsive to all screen sizes.

--- src/components/home-projects.js
+++ src/components/home-projects.js
@@ -1,3 +1,9 @@
 export default function HomeProjects() {
-  return <section className="relative h-screen w-full bg-[#123233]"></section>;
+  return (
+    <section className="relative h-screen w-full bg-[#123233]">
+      <h2 className="pt-5 text-center text-2xl font-semibold text-white underline xs:text-3xl md:pt-10 md:text-4xl lg:text-5xl 2xl:text-6xl">
+        Projects
+      </h2>
+    </section>
+  );
 }
