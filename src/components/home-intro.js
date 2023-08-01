10c5bb465d7a4d97a24f434014c535d5340f0ff5
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-01 13:52:09 +0100

    Refactor home-intro nesting
    Remove an unnecessary layer of nesting. Delete the div that encapsulates all content and move it's styles to the parent section.

--- src/components/home-intro.js
+++ src/components/home-intro.js
@@ -4,27 +4,25 @@ import cover from 'public/home/green-waves-cover.jpg';
 
 export default function HomeIntro() {
   return (
-    <section>
-      <div className="relative h-screen w-full">
-        <Image
-          src={cover}
-          alt="Abstract image showing wavy lines on a dark green background."
-          fill
-          className="z-0 object-cover"
-          sizes="100vw"
+    <section className="relative h-screen w-full">
+      <Image
+        src={cover}
+        alt="Abstract image showing wavy lines on a dark green background."
+        fill
+        className="z-0 object-cover"
+        sizes="100vw"
+      />
+      <div className="absolute mt-[30vh] flex w-full flex-col items-center p-4 text-center text-white sm:mt-[35vh]">
+        <h1 className="bg-gradient-to-r from-teal-200 to-teal-600 bg-clip-text pb-1.5 text-4xl font-bold text-transparent xs:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl">
+          Marley Hoban
+        </h1>
+        <h3 className="my-5 text-xl font-light sm:my-10 md:text-2xl lg:text-3xl 2xl:text-4xl">
+          Mechanical and Manufacturing Engineer
+        </h3>
+        <FaArrowCircleDown
+          size={50}
+          className="transition duration-500 hover:cursor-pointer hover:fill-teal-500"
         />
-        <div className="absolute mt-[30vh] flex w-full flex-col items-center p-4 text-center text-white sm:mt-[35vh]">
-          <h1 className="bg-gradient-to-r from-teal-200 to-teal-600 bg-clip-text pb-1.5 text-4xl font-bold text-transparent xs:text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl">
-            Marley Hoban
-          </h1>
-          <h3 className="my-5 text-xl font-light sm:my-10 md:text-2xl lg:text-3xl 2xl:text-4xl">
-            Mechanical and Manufacturing Engineer
-          </h3>
-          <FaArrowCircleDown
-            size={50}
-            className="transition duration-500 hover:cursor-pointer hover:fill-teal-500"
-          />
-        </div>
       </div>
     </section>
   );
