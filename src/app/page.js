a146aae0fb5e4ec54aae3a097f68d58414b53d45
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-01 13:49:25 +0100

    Implement home-projects on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import HomeIntro from '@/components/home-intro';
+import HomeProjects from '@/components/home-projects';
 
 export default function Home() {
   return (
     <main>
       <HomeIntro />
+      <HomeProjects />
     </main>
   );
 }
