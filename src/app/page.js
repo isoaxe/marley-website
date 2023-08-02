a2ec2c8a3bb575e2494f4b864c0b8577e0216b43
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-02 20:31:36 +0100

    Create path to linkedin from icon
    Navigates to LinkedIn when clicked.

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import { FaLinkedin } from 'react-icons/fa';
+import Link from 'next/link';
 import HomeIntro from '@/components/home-intro';
 import HomeProjects from '@/components/home-projects';
 
@@ -7,12 +8,15 @@ export default function Home() {
     <main>
       <HomeIntro />
       <HomeProjects />
-      <div className="flex w-full justify-center bg-home pb-10">
+      <Link
+        href="https://www.linkedin.com/in/marley-hoban-55447a1b0"
+        className="flex w-full justify-center bg-home pb-10"
+      >
         <FaLinkedin
           size={60}
           className="fill-sky-400 transition duration-300 hover:cursor-pointer hover:fill-teal-600"
         />
-      </div>
+      </Link>
     </main>
   );
 }
