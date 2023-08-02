17afd5e6f53dbd41eb515ca1e19cdde529fad356
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-02 19:57:03 +0100

    Add linkedin icon to bottom of homepage
    Nicely styled icon centred on the bottom of the page. Add transition effect to the colour when hovered.

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import { FaLinkedin } from 'react-icons/fa';
 import HomeIntro from '@/components/home-intro';
 import HomeProjects from '@/components/home-projects';
 
@@ -6,6 +7,12 @@ export default function Home() {
     <main>
       <HomeIntro />
       <HomeProjects />
+      <div className="flex w-full justify-center bg-home pb-10">
+        <FaLinkedin
+          size={60}
+          className="fill-sky-400 transition duration-300 hover:cursor-pointer hover:fill-teal-600"
+        />
+      </div>
     </main>
   );
 }
