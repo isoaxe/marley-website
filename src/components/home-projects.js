567639b0e3ffe3a84392a42bf648849fdb025a71
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-02 11:49:31 +0100

    Add homepage background colour to tailwind config
    Replace with the hardcoded value used in two locations previously as a refactoring.

--- src/components/home-projects.js
+++ src/components/home-projects.js
@@ -3,7 +3,7 @@ import photo from 'public/home/geo-pattern-bg.jpg';
 
 export default function HomeProjects() {
   return (
-    <section className="relative h-fit w-full bg-[#123233]">
+    <section className="relative h-fit w-full bg-home">
       <h2
         id="projects"
         className="pt-5 text-center text-2xl font-semibold text-white underline xs:text-3xl md:pt-10 md:text-4xl lg:text-5xl 2xl:text-6xl"
--- src/components/home-tile.js
+++ src/components/home-tile.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function HomeTile({ photo }) {
   return (
     <div className="w-full p-2 hover:cursor-pointer sm:w-1/2 xl:w-1/3">
-      <div className="relative aspect-[4/3] rounded-lg border-2 border-[#123233] hover:border-teal-700 hover:shadow-xl">
+      <div className="relative aspect-[4/3] rounded-lg border-2 border-home hover:border-teal-700 hover:shadow-xl">
         <Image
           src={photo}
           alt="Cover photo for one of the projects."
