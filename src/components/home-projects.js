b4451a8827e34fce8e25fdc4acc1a9c63c316199
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-02 11:44:47 +0100

    Tweak tile styles and make responsive on homepage
    Make the tile styling better and fix layout.

--- src/components/home-projects.js
+++ src/components/home-projects.js
@@ -3,14 +3,14 @@ import photo from 'public/home/geo-pattern-bg.jpg';
 
 export default function HomeProjects() {
   return (
-    <section className="relative h-screen w-full bg-[#123233]">
+    <section className="relative h-fit w-full bg-[#123233]">
       <h2
         id="projects"
         className="pt-5 text-center text-2xl font-semibold text-white underline xs:text-3xl md:pt-10 md:text-4xl lg:text-5xl 2xl:text-6xl"
       >
         Projects
       </h2>
-      <div className="mt-10 flex max-w-7xl md:mt-16">
+      <div className="m-auto mt-10 flex w-full max-w-[100rem] flex-row flex-wrap px-2 pb-20 md:mt-16 lg:px-6">
         <HomeTile photo={photo} />
         <HomeTile photo={photo} />
         <HomeTile photo={photo} />
--- src/components/home-tile.js
+++ src/components/home-tile.js
@@ -2,14 +2,16 @@ import Image from 'next/image';
 
 export default function HomeTile({ photo }) {
   return (
-    <div className="relative aspect-video w-1/3">
-      <Image
-        src={photo}
-        alt="Cover photo for one of the projects."
-        fill
-        className="z-0 object-cover"
-        sizes="100vw"
-      />
+    <div className="w-full p-2 hover:cursor-pointer sm:w-1/2 xl:w-1/3">
+      <div className="relative aspect-[4/3] rounded-lg border-2 border-[#123233] hover:border-teal-700 hover:shadow-xl">
+        <Image
+          src={photo}
+          alt="Cover photo for one of the projects."
+          fill
+          className="z-0 object-cover"
+          sizes="100vw"
+        />
+      </div>
     </div>
   );
 }
