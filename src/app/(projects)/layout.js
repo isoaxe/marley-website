3868eead99b72b899988d50cef65d3cb7c976402
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-03 21:11:31 +0100

    Wrap home icon in link component
    This allow the user to navigate back to the homepage when clicking on the home icon.

--- src/app/(projects)/layout.js
+++ src/app/(projects)/layout.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { FaHome } from 'react-icons/fa';
 import cover from 'public/projects/abstract-green.jpg';
+import Link from 'next/link';
 
 export default function ProjectLayout({ children }) {
   return (
@@ -13,10 +14,12 @@ export default function ProjectLayout({ children }) {
         sizes="100vw"
       />
       <div className="relative flex flex-row">
-        <FaHome
-          size={50}
-          className="fill-sky-200 transition duration-300 hover:cursor-pointer hover:fill-sky-500"
-        />
+        <Link href="/">
+          <FaHome
+            size={50}
+            className="fill-sky-200 transition duration-300 hover:cursor-pointer hover:fill-sky-500"
+          />
+        </Link>
       </div>
       {children}
     </section>
