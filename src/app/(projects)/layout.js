ede3e127a8094b5ea33cb9d7a4daebe6c5abc247
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-03 21:21:41 +0100

    Add next project arrow and text to layout
    This is a button with associated text located in the top right of the projects layout. It will be visible on every project page.

--- src/app/(projects)/layout.js
+++ src/app/(projects)/layout.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { FaHome } from 'react-icons/fa';
+import { FaHome, FaArrowCircleRight } from 'react-icons/fa';
 import cover from 'public/projects/abstract-green.jpg';
 import Link from 'next/link';
 
@@ -13,13 +13,22 @@ export default function ProjectLayout({ children }) {
         className="absolute z-0 object-cover"
         sizes="100vw"
       />
-      <div className="relative flex flex-row">
+      <div className="relative flex flex-row justify-between">
         <Link href="/">
           <FaHome
             size={50}
             className="fill-sky-200 transition duration-300 hover:cursor-pointer hover:fill-sky-500"
           />
         </Link>
+        <div className="flex flex-row items-center">
+          <h3 className="mr-5 text-3xl font-extralight text-sky-200">
+            Next Project
+          </h3>
+          <FaArrowCircleRight
+            size={50}
+            className="fill-sky-200 transition duration-300 hover:cursor-pointer hover:fill-sky-500"
+          />
+        </div>
       </div>
       {children}
     </section>
