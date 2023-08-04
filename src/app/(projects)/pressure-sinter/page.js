0dcbfa670cddeef07404fe543634f29b7d74547a
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-04 13:42:31 +0100

    Add project location text for psm page
    Text gets displayed at the top of the page with a link to the company website.

--- src/app/(projects)/pressure-sinter/page.js
+++ src/app/(projects)/pressure-sinter/page.js
@@ -5,9 +5,18 @@ export default function PressureSinter() {
     <main className="relative h-screen">
       <ProjectHeading name="Pressure Sintering Machine" />
       <section className="relative px-3 text-white xs:px-6">
-        <h2 className="text-3xl font-semibold md:hidden">
+        <h2 className="mb-5 text-center text-3xl font-semibold md:hidden">
           Pressure Sintering Machine
         </h2>
+        <h3 className="text-center text-2xl text-sky-200 hover:text-sky-500 md:text-left">
+          <a
+            href="https://www.citc.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Chip Integration Technology Center
+          </a>
+        </h3>
       </section>
     </main>
   );
