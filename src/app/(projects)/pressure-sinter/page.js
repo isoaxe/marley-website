f74de7eab384c99d076e2c093b5634d185816c20
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-05 14:05:31 +0100

    Add text to psm page
    Add the three text components that are arranged in a row on larger screens and a column on smaller ones.

--- src/app/(projects)/pressure-sinter/page.js
+++ src/app/(projects)/pressure-sinter/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import ProjectHeading from '@/components/project-heading';
+import ProjectText from '@/components/project-text';
+import { psmText } from '@/util/text-data';
 import mainImg from 'public/projects/psm/completed-machine.png';
 
 export default function PressureSinter() {
   return (
     <main className="relative h-fit">
       <ProjectHeading name="Pressure Sintering Machine" />
-      <section className="relative px-3 text-white xs:px-6">
+      <section className="relative px-3 pb-6 text-white xs:px-6">
         <h2 className="mb-5 text-center text-3xl font-semibold md:hidden">
           Pressure Sintering Machine
         </h2>
@@ -19,7 +21,7 @@ export default function PressureSinter() {
             Chip Integration Technology Center
           </a>
         </h3>
-        <div className="relative mt-8 h-72 w-full sm:h-96">
+        <div className="relative my-8 h-72 w-full sm:h-96">
           <Image
             src={mainImg}
             alt="Photo of the pressure sintering machine."
@@ -27,6 +29,11 @@ export default function PressureSinter() {
             className="z-10 object-contain"
             sizes="100vw"
           />
+        </div>
+        <div className="flex w-full max-w-[90rem] flex-row flex-wrap justify-between">
+          <ProjectText text={psmText.overview} />
+          <ProjectText text={psmText.method} />
+          <ProjectText text={psmText.result} />
         </div>
       </section>
     </main>
