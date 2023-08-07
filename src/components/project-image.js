e997f9604063f6b6491cfd651e5f9c91854f532c
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-07 20:24:21 +0100

    Make project-image corners rounded
    To match the style of project-text.

--- src/components/project-image.js
+++ src/components/project-image.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function ProjectImage({ image, caption }) {
   return (
     <div className="w-full md:my-8 md:w-1/2 lg:w-1/3">
-      <div className="relative m-2 h-72 sm:h-96">
+      <div className="relative m-2 h-72 overflow-hidden rounded sm:h-96">
         <Image
           src={image}
           alt={caption}
