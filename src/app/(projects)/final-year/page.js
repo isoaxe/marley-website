ee8b187442aec5defae655366ec913c0b68b50f0
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-08 20:37:29 +0100

    Tweak final year project heading styles
    Make same as the pressure-sinter page. More responsive etc. No additional content added though.

--- src/app/(projects)/final-year/page.js
+++ src/app/(projects)/final-year/page.js
@@ -4,8 +4,10 @@ export default function FinalYear() {
   return (
     <main className="relative h-screen">
       <ProjectHeading name="Final Year Project" />
-      <section className="relative px-3 text-white xs:px-6">
-        <h2 className="text-3xl font-semibold md:hidden">Final Year Project</h2>
+      <section className="relative px-3 pb-6 text-white xs:px-6">
+        <h2 className="mb-5 text-center text-3xl font-semibold md:hidden">
+          Final Year Project
+        </h2>
       </section>
     </main>
   );
