b6539e215364a37956777bc58c596e14538321a8
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-10 09:00:36 +0100

    Add main image to audio-visual page
    Photo of Marley under the installation. Made a third larger than the cover images on other pages due to its slenderness.

--- src/app/(projects)/audio-visual/page.js
+++ src/app/(projects)/audio-visual/page.js
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import ProjectHeading from '@/components/project-heading';
+import mainImg from 'public/projects/avi/external-photo.png';
 
 export default function AudioVisual() {
   return (
-    <main className="relative h-screen">
+    <main className="relative h-fit">
       <ProjectHeading name="Audio-Visual Installation" />
       <section className="relative px-3 pb-6 text-white xs:px-6">
         <h2 className="mb-5 text-center text-3xl font-semibold md:hidden">
@@ -17,6 +19,15 @@ export default function AudioVisual() {
             E.L.G. Arts Festival
           </a>
         </h3>
+        <div className="relative h-96 w-full xs:h-[32rem]">
+          <Image
+            src={mainImg}
+            alt="External structure with fabric sleeve"
+            fill
+            className="z-10 object-contain"
+            sizes="100vw"
+          />
+        </div>
       </section>
     </main>
   );
