7862b454ef2dfc2a665fc1a45abade2543d6a9cb
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-08-10 08:12:36 +0100

    Tweak final year project heading styles
    Make same as the other pages. More responsive etc. No additional content added.

--- src/app/(projects)/audio-visual/page.js
+++ src/app/(projects)/audio-visual/page.js
@@ -4,8 +4,8 @@ export default function AudioVisual() {
   return (
     <main className="relative h-screen">
       <ProjectHeading name="Audio-Visual Installation" />
-      <section className="relative px-3 text-white xs:px-6">
-        <h2 className="text-3xl font-semibold md:hidden">
+      <section className="relative px-3 pb-6 text-white xs:px-6">
+        <h2 className="mb-5 text-center text-3xl font-semibold md:hidden">
           Audio-Visual Installation
         </h2>
       </section>
